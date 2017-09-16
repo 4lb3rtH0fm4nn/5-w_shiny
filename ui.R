8f7d6d57a4a8266891bdd44d2fc4bbd432c818fb
4lb3rtH0fm4nn <marcell.gal@gmail.com>
2017-09-17 01:17:16 +0200

    asd
    asd

--- ui.R
+++ ui.R
@@ -14,53 +14,47 @@ library(plotly)
 library(igraph)
 shinyUI(
   navbarPage("Five words analytics",
-              
-             tabPanel(
-                "Wordcloud",
-                      tags$head(tags$script('
-                                                      var dimension = [0, 0];
-                                                      $(document).on("shiny:connected", function(e) {
-                                                      dimension[0] = window.innerWidth;
-                                                      dimension[1] = window.innerHeight;
-                                                      Shiny.onInputChange("dimension", dimension);
-                                                      });
-                                                      $(window).resize(function(e) {
-                                                      dimension[0] = window.innerWidth;
-                                                      dimension[1] = window.innerHeight;
-                                                      Shiny.onInputChange("dimension", dimension);
-                                                      });
-                                                      ')),
+             tabPanel(                "Introduction"),
+             
+             tabPanel(                "Wordcloud",
+                                      
+                                      tags$head(tags$script('
+                                                            var dimension = [0, 0];
+                                                            $(document).on("shiny:connected", function(e) {
+                                                            dimension[0] = window.innerWidth;
+                                                            dimension[1] = window.innerHeight;
+                                                            Shiny.onInputChange("dimension", dimension);
+                                                            });
+                                                            $(window).resize(function(e) {
+                                                            dimension[0] = window.innerWidth;
+                                                            dimension[1] = window.innerHeight;
+                                                            Shiny.onInputChange("dimension", dimension);
+                                                            });
+                                                            ')),
                 radioButtons("wc2type", "Wordcloud type",
                              choices = c("non-stemmed", "stemmed"),
                              selected = "non-stemmed"),
                 numericInput('size', 'Size of wordcloud', 100),
                wordcloud2Output('wordcloud2')
              ),
-             tabPanel("asd",
-               sidebarPanel(sliderInput("priceInput", "Price", min = 0, max = 100,
-                                        value = c(25, 40), pre = "$"),
-                            radioButtons("typeInput", "Product type",
-                                         choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                                         selected = "WINE"),
-                            selectInput("countryInput", "Country",
-                                        choices = c("CANADA", "FRANCE", "ITALY"))),
-               mainPanel(verbatimTextOutput("height"),
-                         br(),
-                         br(),
-                         tableOutput("results")
-               )
+
+             tabPanel("non-stemmed rules plot",
+                      sidebarPanel(width=3,
+                        sliderInput("in", "Price", min = 0, max = 100,
+                                               value = c(25, 40), pre = "$"),
+                        radioButtons("rulesplottype", "Stemming",
+                                    choices = c("non-stemmed", "stemmed"),
+                                    selected = "non-stemmed"),
+                        selectInput("rulesx", "X axis",
+                                    choices = c("support", "confidence", "lift","order")),
+                        selectInput("rulesy", "y axis",
+                                    choices = c("support", "confidence", "lift","order"),selected = "confidence"),
+                        selectInput("rulesshade", "shading",
+                                    choices = c("support", "confidence", "lift","order"),selected = "lift")
+                        ),
+                      mainPanel(plotlyOutput("plotrules2")
+                      )
                       
-                      
-                      ),
-             tabPanel("Component 3",
-                      plotlyOutput("plotrules2")
-                      
-                      
-                      ),
-             tabPanel("Component 4",
-                      plotlyOutput("plotrules2_stemmed")
-                      
-                      
-             )
+                      )
   )
 )
